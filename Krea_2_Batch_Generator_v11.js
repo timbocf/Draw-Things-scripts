7733a3d8ee7debad8f5fbeaad50a8eb6fee0bdc7
Tim Rodgers <timbocf@gmail.com>
2026-09-10 23:47:44 -0700

    Refine celebrity presets with improved descriptions and adjust age selection default

--- Krea_2_Batch_Generator_v11.js
+++ Krea_2_Batch_Generator_v11.js
@@ -52,7 +52,7 @@ const celebrityPresets = [
     { label: "Marilyn Monroe", value: "Marilyn Monroe with shoulder length blonde Hollywood curls" },
     { label: "Lisbeth Salander", value: "small petite woman with porcelain skin, a flat chest, narrow hips/shoulders, a short black spiked punk hairstyle shaved on one side, neck tattoos, back tattoos, light body hair, arm and leg tattoos, stacked bracelets, heavy mascara, smokey eyes, eyebrow/lip/septum/nipple/navel piercings, multiple earrings, multiple rings" },
     { label: "Curvy Black Woman with Box Braids", value: "a curvy 35-year-old black woman with warm brown skin, long black box braids, neck/back/arm tattoos, heavy mascara, smokey eyes, light body hair, hoop earrings, long fingernails, nose/navel/nipple piercings" },
-    { label: "Curvy 25-year-old Mexican woman with prominent Indigenous Mesoamerican features, olive skin, medium-length straight black hair, smokey eyes, heavy mascara, arm/back/neck tattoos, light body hair, hoop earrings, multiple rings, nose/navel/nipple piercings" }
+    { label: "Curvy 25-year-old Mexican woman", value: "a curvy 25-year-old Mexican woman with prominent Indigenous Mesoamerican features, olive skin, medium-length straight black hair, smokey eyes, heavy mascara, arm/back/neck tattoos, light body hair, hoop earrings, multiple rings, nose/navel/nipple piercings" }
 ];
 
 // --- AGE ---
@@ -822,7 +822,7 @@ const inputs = requestFromUser("Batch Prompts", "Generate", function () {
             [
                 this.menu(NONE_SELECTED, menuWithPlaceholder("Choose gender", genderPresets)),
                 this.menu(NONE_SELECTED, menuWithPlaceholder("Choose nationality / ethnicity", nationalityPresets)),
-                this.menu(NONE_SELECTED, agePresets),
+                this.menu(agePresets.indexOf("30 years old"), agePresets),
                 this.menu(NONE_SELECTED, menuWithPlaceholder("Choose skin tone", skinTonePresets))
             ]
         ));
