004ffe86ff43a65a37c692f9b1f73813cfb045be
Tim Rodgers <timbocf@gmail.com>
2026-09-19 14:27:24 -0700

    Add new celebrity preset "My Baby" with detailed description

--- Krea_2_Batch_Generator_v12.js
+++ Krea_2_Batch_Generator_v12.js
@@ -154,6 +154,7 @@ const nationalityPresets = [
 ];
 
 const celebrityPresets = [
+    { label: "My Baby", value: "curvy apple-shaped 35-year-old woman with large shapeless drooping breasts, a round ass, long curly 3a black hair, arms covered in red & green rose tattoos, hazel eyes" },
     { label: "Anne Hathaway", value: "Anne Hathaway with a tall slim build with smokey eyes and heavy mascara" },
     { label: "Dolly Parton", value: "young 1970s era Dolly Parton with blown-out blonde hair and bangs" },
     { label: "Sabrina Carpenter", value: "Sabrina Carpenter with shoulder length blonde hair" },
