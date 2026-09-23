d8526841260d9708d766506795e5f497f76e445a
Tim Rodgers <timbocf@gmail.com>
2026-09-23 01:04:04 -0700

    Debug 4

--- randomizer.js
+++ randomizer.js
@@ -377,7 +377,7 @@ async function generateBatch() {
     }
 
 
-    const imagePrompt = "A photo of a " + age + " " + nationality.label + " " + gender + " with " + skinTone + " skin, " + hairColor + " hair and " + nationality.value + ", " + action + ", wearing " + outfit + ". She has a " + overallBuild.value + " and " + eyeColor.value + ". Natural anatomy.";
+    const imagePrompt = "A photo of a " + age + " " + nationality.label + " " + gender + " with " + skinTone + " skin, " + hairColor + " hair and " + nationality.value + ", " + action + ", wearing " + outfit + ". She has a " + overallBuild.value + " and " + eyeColor + ". Natural anatomy.";
     console.log("Generating:");
     console.log(imagePrompt);
 
